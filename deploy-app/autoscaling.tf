c35c2ea7841cdcd09b579d0172a618c065e782f6
Ramprasad-R <ramprasad.info@gmail.com>
2021-01-17 00:32:01 +0100

    created tf for autoscaling, lauch config, asg policy

--- deploy-app/autoscaling.tf
+++ deploy-app/autoscaling.tf
@@ -1,1 +1,39 @@
-#
+resource "aws_launch_configuration" "magento-launchconfig" {
+  name_prefix     = "magento-launchconfig-"
+  image_id        = var.MAGENTO_INSTANCE_AMI
+  instance_type   = "t3a.medium"
+  key_name        = var.ssh_key_name
+  security_groups = [var.magento_ec2_sg]
+  iam_instance_profile = var.magento_instance_role
+  lifecycle {
+    create_before_destroy = true
+  }
+  root_block_device {
+      volume_size = "30"
+      volume_type = "gp2"
+    }
+  }
+
+resource "aws_autoscaling_group" "magento-autoscaling" {
+  name                      = "magento-autoscaling"
+  vpc_zone_identifier       = [var.private_subnet_1, var.private_subnet_2, var.private_subnet_3]
+  launch_configuration      = aws_launch_configuration.magento-launchconfig.name
+  min_size                  = 1
+  max_size                  = 2
+  health_check_grace_period = 300
+  health_check_type         = "ELB"
+  force_delete              = true
+  tag {
+    key                 = "Name"
+    value               = "Magento Ec2 instance"
+    propagate_at_launch = true
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_attachment" "magento-asg-attachment" {
+  autoscaling_group_name = aws_autoscaling_group.magento-autoscaling.id
+  alb_target_group_arn   = var.target_group_arn
+}
--- deploy-app/autoscalingpolicy.tf
+++ deploy-app/autoscalingpolicy.tf
@@ -1,17 +1,15 @@
-# scale up alarm
-
-resource "aws_autoscaling_policy" "example-cpu-policy" {
-  name                   = "example-cpu-policy"
-  autoscaling_group_name = aws_autoscaling_group.example-autoscaling.name
+resource "aws_autoscaling_policy" "magento-cpu-policy" {
+  name                   = "magento-cpu-policy"
+  autoscaling_group_name = aws_autoscaling_group.magento-autoscaling.name
   adjustment_type        = "ChangeInCapacity"
   scaling_adjustment     = "1"
   cooldown               = "300"
   policy_type            = "SimpleScaling"
 }
 
-resource "aws_cloudwatch_metric_alarm" "example-cpu-alarm" {
-  alarm_name          = "example-cpu-alarm"
-  alarm_description   = "example-cpu-alarm"
+resource "aws_cloudwatch_metric_alarm" "magento-cpu-alarm" {
+  alarm_name          = "magento-cpu-alarm"
+  alarm_description   = "magento-cpu-alarm"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = "2"
   metric_name         = "CPUUtilization"
@@ -21,26 +19,25 @@ resource "aws_cloudwatch_metric_alarm" "example-cpu-alarm" {
   threshold           = "30"
 
   dimensions = {
-    "AutoScalingGroupName" = aws_autoscaling_group.example-autoscaling.name
+    "AutoScalingGroupName" = aws_autoscaling_group.magento-autoscaling.name
   }
 
   actions_enabled = true
-  alarm_actions   = [aws_autoscaling_policy.example-cpu-policy.arn]
+  alarm_actions   = [aws_autoscaling_policy.magento-cpu-policy.arn]
 }
 
-# scale down alarm
-resource "aws_autoscaling_policy" "example-cpu-policy-scaledown" {
-  name                   = "example-cpu-policy-scaledown"
-  autoscaling_group_name = aws_autoscaling_group.example-autoscaling.name
+resource "aws_autoscaling_policy" "magento-cpu-policy-scaledown" {
+  name                   = "magento-cpu-policy-scaledown"
+  autoscaling_group_name = aws_autoscaling_group.magento-autoscaling.name
   adjustment_type        = "ChangeInCapacity"
   scaling_adjustment     = "-1"
   cooldown               = "300"
   policy_type            = "SimpleScaling"
 }
 
-resource "aws_cloudwatch_metric_alarm" "example-cpu-alarm-scaledown" {
-  alarm_name          = "example-cpu-alarm-scaledown"
-  alarm_description   = "example-cpu-alarm-scaledown"
+resource "aws_cloudwatch_metric_alarm" "magento-cpu-alarm-scaledown" {
+  alarm_name          = "magento-cpu-alarm-scaledown"
+  alarm_description   = "magento-cpu-alarm-scaledown"
   comparison_operator = "LessThanOrEqualToThreshold"
   evaluation_periods  = "2"
   metric_name         = "CPUUtilization"
@@ -50,10 +47,10 @@ resource "aws_cloudwatch_metric_alarm" "example-cpu-alarm-scaledown" {
   threshold           = "5"
 
   dimensions = {
-    "AutoScalingGroupName" = aws_autoscaling_group.example-autoscaling.name
+    "AutoScalingGroupName" = aws_autoscaling_group.magento-autoscaling.name
   }
 
   actions_enabled = true
-  alarm_actions   = [aws_autoscaling_policy.example-cpu-policy-scaledown.arn]
+  alarm_actions   = [aws_autoscaling_policy.magento-cpu-policy-scaledown.arn]
 }
 
